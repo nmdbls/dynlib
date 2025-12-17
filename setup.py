39b94f6bf8dbdc79d5ce8ee92e715dc77160c0c4
nmdbls <gordeevarsenij281@gmail.com>
2025-12-17 23:01:35 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, Extension
-import sys
-
-extra_compile_args = []
-if sys.platform != "win32":
-    extra_compile_args = ["-std=c99", "-O3"]
 
 module = Extension(
     "mylib",
     sources=["main.c"],
-    extra_compile_args=extra_compile_args,
 )
 
 setup(name="mylib", version="0.1", ext_modules=[module])
